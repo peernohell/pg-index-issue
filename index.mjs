c697868cb70093d0aabe563b7b74dca4c593bac9
Francois Lecroart <peernohell@gmail.com>
2021-05-02 19:16:10 +0200

    fix how createdAt is generated + generate more data

--- index.mjs
+++ index.mjs
@@ -19,21 +19,28 @@ const pool = new Pool({
 // })
 const r = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-const insert = async () => {
-  const res = await pool.query({
-    text:
-    `
-    INSERT INTO "activities" ("_id", "createdAt", "extra")
-    VALUES ($1, $2, $3);
-    `,
-    values: [r().slice(0, 21), new Date(startingAt + (Date.now() - startingAt * Math.random())), { info: Array.from({ length: (1 + Math.random() * 10 | 0) }).map(r).join(' ') }],
-  })
+const insert = text => pool.query({ text });
+
+let createdAt = startingAt
+const buildInsert = () => {
+  const id = r().slice(0, 21);
+  createdAt += 10_000;
+  const info = Array.from({ length: (1 + Math.random() * 10 | 0) }).map(r).join(' ')
+
+  return `
+  INSERT INTO "activities" ("_id", "createdAt", "info", "extra")
+  VALUES ('${id}', to_timestamp(${createdAt / 1000 | 0}), '${info}', '{"info": ${JSON.stringify({ info })} }');
+  `
 }
+
+const buildXInsert = length => Array.from({ length }).map(buildInsert).join('\n');
 
 let i = 4;
 console.log('starting inserting data')
 while (i--) {
-  await Promise.all(Array.from({ length: 1000 }).map(insert))
+  // await Promise.all(Array.from({ length: 1000 }).map(buildInsert).map(insert))
+  // await insert(buildXInsert(1000))
+  await Promise.all(Array.from({ length: 10 }).map(() => buildXInsert(1000)).map(insert))
   console.log(`iteration ${i}`)
 }
 
